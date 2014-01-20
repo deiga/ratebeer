68e241fa3d474be2fbe34f2d55b121e7f12055c6
Timo Sand <timo.j.sand@gmail.com>
2014-01-20 20:23:31 +0200

    Prettier avergae code

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -3,6 +3,7 @@ class Beer < ActiveRecord::Base
   has_many :ratings
 
   def average_rating
-    ratings.inject(0) { |sum, elem| sum + elem.score }/ratings.count unless ratings.empty?
+    ratings.average('score')
+    #ratings.inject(0) { |sum, elem| sum + elem.score }/ratings.count unless ratings.empty?
   end
 end
