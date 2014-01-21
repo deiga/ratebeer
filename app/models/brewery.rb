c89d609ff9f7fce7c1c4bc77e1aef5bad2a50978
Timo Sand <timo.j.sand@gmail.com>
2014-01-21 10:35:18 +0200

    EX14

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,9 @@
 class Brewery < ActiveRecord::Base
   has_many :beers, dependent: :destroy
+  has_many :ratings, through: :beers
+
+  def average_rating
+    ratings.average('score')
+    #ratings.inject(0) { |sum, elem| sum + elem.score }/ratings.count unless ratings.empty?
+  end
 end
