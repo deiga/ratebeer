47f31e5acd546aaeed247f61aafdf7d98ae97892
Timo Sand <timo.j.sand@gmail.com>
2014-01-26 09:51:46 +0200

    EX5

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,5 +1,7 @@
 class Beer < ActiveRecord::Base
   include RatingAverage
+
+  validates :name, presence: :true
   belongs_to :brewery
   has_many :ratings, dependent: :destroy
 
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,5 +1,9 @@
 class Brewery < ActiveRecord::Base
   include RatingAverage
+
+  validates :name, presence: :true
+  validates :year, numericality: { only_integer: true, less_than: 2015, greater_than: 1041}
+
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
 
