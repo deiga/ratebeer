fce0aa8e0d75eb8cae514d35324604136cad0ded
Timo Sand <timo.j.sand@gmail.com>
2014-01-26 12:14:24 +0200

    EX11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,8 +40,9 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
+    updated = @user.update(user_params) if @user == current_user
     respond_to do |format|
-      if @user.update(user_params)
+      if updated
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { render action: 'show', status: :ok, location: @user }
       else
@@ -54,7 +55,7 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy
+    @user.destroy if @user == current_user
     respond_to do |format|
       format.html { redirect_to users_url }
       format.json { head :no_content }
