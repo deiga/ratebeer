5c0f651b39b6114dc713493bf7abd491d09c38f1
Timo Sand <timo.j.sand@gmail.com>
2014-01-26 09:56:32 +0200

    EX6

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -4,6 +4,7 @@ class Beer < ActiveRecord::Base
   validates :name, presence: :true
   belongs_to :brewery
   has_many :ratings, dependent: :destroy
+  has_many :raters, -> { uniq }, through: :ratings, source: :user
 
   def to_s
     "#{name} - #{brewery.name}"
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,8 +1,10 @@
+require 'date'
+
 class Brewery < ActiveRecord::Base
   include RatingAverage
 
   validates :name, presence: :true
-  validates :year, numericality: { only_integer: true, less_than: 2015, greater_than: 1041}
+  validates :year, numericality: { only_integer: true, less_than_or_equal_to: lambda {|x| Date.today.year }, greater_than_or_equal_to: 1042}
 
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
