c632f309f843108e9cd9edd6137c02c4b2e5d993
Timo Sand <timo.j.sand@gmail.com>
2014-02-09 17:44:33 +0200

    EX2 done

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -2,6 +2,7 @@ class Beer < ActiveRecord::Base
   include RatingAverage
 
   validates :name, presence: :true
+  validates :style, presence: :true
   belongs_to :brewery
   has_many :ratings, dependent: :destroy
   has_many :raters, -> { uniq }, through: :ratings, source: :user
--- spec/models/beer_spec.rb
+++ spec/models/beer_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe Beer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is not saved without name" do
+    beer = Beer.create style:'Lager'
+
+    beer.should_not be_valid
+    expect(Beer.count).to eq(0)
+  end
+
+  it "is not saved without style" do
+    beer = Beer.create name:'Lager'
+
+    beer.should_not be_valid
+    expect(Beer.count).to eq(0)
+
+  end
+
+  describe "with name and style" do
+    let(:beer) {Beer.create name:'foo', style:'bar'}
+
+    it "is saved" do
+      beer.should be_valid
+      Beer.count.should eq(1)
+    end
+  end
+
 end
