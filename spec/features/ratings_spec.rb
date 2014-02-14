320f870ffd8f6cec8a5f47c470d5259f2ec3dd8f
Timo Sand <timo.j.sand@gmail.com>
2014-02-14 23:09:21 +0200

    W4EX8

--- spec/features/ratings_spec.rb
+++ spec/features/ratings_spec.rb
@@ -19,33 +19,45 @@ describe "Rating" do
 
     expect{
       click_button "Create Rating"
-      }.to change{Rating.count}.from(0).to(1)
+    }.to change{Rating.count}.from(0).to(1)
 
 
-      expect(user.ratings.count).to eq(1)
-      expect(beer1.ratings.count).to eq(1)
-      expect(beer1.average_rating).to eq(15.0)
-    end
-
-
-
-    it "number of rataings on list page is correct" do
-      FactoryGirl.create :rating, beer: beer1, user: user
-      FactoryGirl.create :rating, beer: beer1, user: user
-      FactoryGirl.create :rating, beer: beer2, user: user
-
-      visit ratings_path
-
-      page.should have_content "Number of ratings: #{Rating.count}"
-    end
-
-    it "number of rataings on users page is correct" do
-      FactoryGirl.create :rating, beer: beer1, user: user
-      FactoryGirl.create :rating, beer: beer1, user: user2
-      FactoryGirl.create :rating, beer: beer2, user: user2
-
-      visit user_path(user)
-
-      page.should have_content "Has #{user.ratings.count} rating"
-    end
+    expect(user.ratings.count).to eq(1)
+    expect(beer1.ratings.count).to eq(1)
+    expect(beer1.average_rating).to eq(15.0)
   end
+
+
+    describe "is listed correctly" do
+      before(:each) do
+        FactoryGirl.create :rating, beer: beer1, user: user
+        FactoryGirl.create :rating, beer: beer1, user: user
+        FactoryGirl.create :rating, beer: beer2, user: user
+      end
+
+      it "number of ratings on list page is correct" do
+        visit ratings_path
+
+        page.should have_content "Number of ratings: #{Rating.count}"
+      end
+
+      it "number of ratings on users page is correct" do
+        visit user_path(user)
+
+        page.should have_content "Has #{user.ratings.count} rating"
+      end
+    end
+
+    describe "interactions are commited to DB" do
+
+      it "User clicks on delete removes for DB" do
+        FactoryGirl.create :rating, beer: beer1, user: user
+
+        visit user_path(user)
+
+        expect {
+          click_link 'delete'
+          }.to change{Rating.count}.from(1).to(0)
+        end
+      end
+    end
