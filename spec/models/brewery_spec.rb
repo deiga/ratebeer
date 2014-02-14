78b4e585fe5c0dda6b3943e89c9bc957ba48ba04
Timo Sand <timo.j.sand@gmail.com>
2014-02-14 23:40:31 +0200

    Added brewery spec

--- spec/models/brewery_spec.rb
+++ spec/models/brewery_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Brewery do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "when initialized with name Schlenkerla and year 1674" do
+    subject{ Brewery.create name: "Schlenkerla", year: 1674 }
+
+    it { should be_valid }
+    its(:name) { should eq("Schlenkerla") }
+    its(:year) { should eq(1674) }
+  end
+
+   it "without a name is not valid" do
+    brewery = Brewery.create  year:1674
+
+    brewery.should_not be_valid
+  end
 end
