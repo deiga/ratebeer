c7e16830d4b86467683e7be72a1825cf3e504314
Timo Sand <timo.j.sand@gmail.com>
2014-02-15 22:03:06 +0200

    EX8

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -30,7 +30,7 @@ class MembershipsController < ApplicationController
 
     respond_to do |format|
       if @membership.save
-        format.html { redirect_to @membership, notice: 'Membership was successfully created.' }
+        format.html { redirect_to @membership.beer_club, notice: "#{@membership.user.username}, welcome to the club!" }
         format.json { render action: 'show', status: :created, location: @membership }
       else
         @beer_clubs = BeerClub.all.reject{ |b| b.members.include? current_user }
