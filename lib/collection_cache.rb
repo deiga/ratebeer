6028020cf72ca00678e503ff04708144bed74764
Timo Sand <timo.j.sand@gmail.com>
2014-02-19 14:09:28 +0200

    Fixed test

--- lib/collection_cache.rb
+++ lib/collection_cache.rb
@@ -7,8 +7,12 @@ module CollectionCache
   module ClassMethods
     def cache_key_collection
       count_n_max = pluck('COUNT(*)', 'MAX(updated_at)').flatten
-      max_updated_at = Time.new(count_n_max.last).try(:utc).try(:to_s, :number)
-      "#{model_name.human.pluralize.downcase}/all-#{count_n_max.first}-#{max_updated_at}"
+      if count_n_max.last
+        max_updated_at = Time.new(count_n_max.last).try(:utc).try(:to_s, :number)
+        "#{model_name.human.pluralize.downcase}/all-#{count_n_max.first}-#{max_updated_at}"
+      else
+        "#{model_name.human.pluralize.downcase}/all-#{count_n_max.first}"
+      end
     end
   end
 
